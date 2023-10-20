f2f3777b1ca909416a8feeb67a990e077d14e100
TancelinMazzotti <tancelin.mazzotti@gmail.com>
2023-10-20 23:21:09 +0200

    Remove useless test

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,10 +1,5 @@
 #include <gtest/gtest.h>
 
-TEST(MyMathTest, AddTest) {
-    EXPECT_EQ(2 + 3, 5);
-    EXPECT_EQ(-1 + 1, 0);
-}
-
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
